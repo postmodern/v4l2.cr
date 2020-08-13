d98654cff6b45448230dc0a4371296dbb51e5c6a
Postmodern <postmodern.mod3@gmail.com>
2020-08-13 09:07:15 -0700

    Renamed PixelFormat to PixFmt.

--- src/v4l2.cr
+++ src/v4l2.cr
@@ -7,7 +7,7 @@ module V4L2
   VERSION = "0.1.0"
 
   alias Field = Linux::V4L2Field
-  alias PixelFormat = Linux::V4L2PixFmt
+  alias PixFmt = Linux::V4L2PixFmt
   alias ColorSpace = Linux::V4L2ColorSpace
   alias XFERFunc = Linux::V4L2XFERFunc
   alias YCBCREncoding = Linux::V4L2YCBCREncoding
