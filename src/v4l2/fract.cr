6da815bff3d388e22a7aa04c55165a94185ce077
Postmodern <postmodern.mod3@gmail.com>
2020-08-15 04:59:04 -0700

    Allow Fract to be initialized via a struct and to return a struct via #to_unsafe.

--- src/v4l2/fract.cr
+++ src/v4l2/fract.cr
@@ -1,5 +1,11 @@
+require "../linux/videodev2"
+
 module V4L2
   record Fract, numerator : UInt32, denominator : UInt32 do
+
+    def initialize(fract_struct : Linux::V4L2Fract)
+      initialize(fract_struct.numerator,fract_struct.denominator)
+    end
 
     def to_f
       numerator.to_f / denominator.to_f
@@ -7,6 +13,13 @@ module V4L2
 
     def to_s
       "#{numerator}/#{denominator}"
+    end
+
+    def to_unsafe : Linux::V4L2Fract
+      fract_struct = Linux::V4L2Fract.new
+      fract_struct.numerator   = numerator
+      fract_struct.denominator = denominator
+      return fract_struct
     end
 
   end
