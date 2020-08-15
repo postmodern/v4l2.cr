54bc19a81c2f4491da71db554b26ae89eeacb4cf
Postmodern <postmodern.mod3@gmail.com>
2020-08-15 04:56:10 -0700

    Added an example of writing previously captured video frames into a v4l2loopback dev.

---
+++ examples/write_mjpeg.cr
@@ -0,0 +1,52 @@
+require "../src/v4l2"
+
+video = ARGV.fetch(0,"/dev/video0")
+
+unless File.exists?(video)
+  puts "Loading v4l2loopback kernel module ..."
+  system "sudo modprobe v4l2loopback"
+end
+
+# sleep 1
+# puts "Starting mpv ..."
+# system "mpv /dev/video1"
+
+begin
+  V4L2::Device.open(video) do |device|
+    device.video_output.format do |format|
+      format.width = 640
+      format.height = 480
+      format.pixel_format = Linux::V4L2PixFmt::MJPEG
+    end
+
+    format = device.video_output.format
+    puts "Format: #{format.pixel_format} #{format.width}x#{format.height}"
+
+    puts "Parm: #{device.video_output.parm.inspect}"
+
+    images  = Dir["image-*.jpg"]
+    buffers = Array(Bytes).new(images.size) do |index|
+      Bytes.new(format.size_image).tap do |buffer|
+        File.open(images[index],"rb") do |file|
+          file.read(buffer)
+        end
+      end
+    end
+
+    puts "Spawning mpv ..."
+    system "mpv #{video} &"
+
+    puts "Streaming ..."
+    loop do
+      buffers.each do |buffer|
+        device.write(buffer)
+      end
+    end
+  end
+rescue error : V4L2::Error
+  STDERR.puts error.message
+  error.backtrace.each do |line|
+    STDERR.puts "\t#{line}"
+  end
+  exit -1
+end
